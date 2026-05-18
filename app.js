45782718a229f9cc0e9a599d4287a65df1f00a33
Fokatloot <ravirathiplus@gmail.com>
2026-05-18 09:06:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
-function registerUser() {
-  alert("Registration Successful");
-  window.location.href = "/dashboard.html";
+function loginUser() {
+
+  alert("Login Successful");
+
+  window.location.href = "dashboard.html";
 }
 
-function loginUser() {
-  alert("Login Successful");
-  window.location.href = "/dashboard.html";
+function registerUser() {
+
+  alert("Registration Successful");
+
+  window.location.href = "dashboard.html";
 }
