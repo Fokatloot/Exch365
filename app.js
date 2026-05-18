14f1c4476d5ae98efd493caed8971f7e8cb0df92
Fokatloot <ravirathiplus@gmail.com>
2026-05-18 09:03:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,9 @@
-function showRegister() {
-
-  document.getElementById("loginScreen").style.display = "none";
-  document.getElementById("registerScreen").style.display = "block";
-}
-
-function showLogin() {
-
-  document.getElementById("registerScreen").style.display = "none";
-  document.getElementById("loginScreen").style.display = "block";
-}
-
 function registerUser() {
-
   alert("Registration Successful");
-
-  window.location.href = "Dashboard.html";
+  window.location.href = "/dashboard.html";
 }
 
 function loginUser() {
-
   alert("Login Successful");
-
-  window.location.href = "Dashboard.html";
+  window.location.href = "/dashboard.html";
 }
